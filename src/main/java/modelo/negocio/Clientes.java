30471e433e30f3b26558b69a02fe0694f7e21412
Jxse111 <josemartinezestrada111@gmail.com>
2023-02-17 10:29:51 +0100

    Modifique las clases Clientes,Turismos y Alquileres porque las duplique y ya he dejado las clases como deberian de estar en sus paquetes correspondientes

--- src/main/java/modelo/negocio/Clientes.java
+++ src/main/java/modelo/negocio/Clientes.java
@@ -1,10 +1,58 @@
 package modelo.negocio;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.dominio.Cliente;
+
 public class Clientes {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private List<Cliente> clientes;
 
+	public Clientes() {
+		clientes = new ArrayList<>();
 	}
 
+	public List<Cliente> get() {
+		return new ArrayList<>(clientes);
+	}
+
+	public int getCantidad() {
+		return clientes.size();
+	}
+
+	public void insertar(Cliente cliente) {
+		if (cliente != null && !clientes.contains(cliente)) {
+			clientes.add(cliente);
+		}
+	}
+
+	public Cliente buscar(Cliente cliente) {
+		int index = clientes.indexOf(cliente);
+		if (index != -1) {
+			return clientes.get(index);
+		}
+		return null;
+	}
+
+	public void borrar(Cliente cliente) throws Exception {
+		if (clientes.contains(cliente)) {
+			clientes.remove(cliente);
+		} else {
+			throw new Exception("El cliente no se encuentra en la lista.");
+		}
+	}
+
+	public void modificar(Cliente cliente, String nuevoNombre, String nuevoTelefono) throws Exception {
+		if (clientes.contains(cliente)) {
+			if (nuevoNombre != null && !nuevoNombre.trim().isEmpty()) {
+				cliente.setNombre(nuevoNombre);
+			}
+			if (nuevoTelefono != null && !nuevoTelefono.trim().isEmpty()) {
+				cliente.setTelefono(nuevoTelefono);
+			}
+		} else {
+			throw new Exception("El cliente no se encuentra en la lista.");
+		}
+	}
 }
--- src/main/java/modelo/negocio/Turismos.java
+++ src/main/java/modelo/negocio/Turismos.java
@@ -1,10 +1,44 @@
 package modelo.negocio;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.dominio.Turismo;
+
 public class Turismos {
+	private List<Turismo> turismos;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Turismos() {
+		turismos = new ArrayList<>();
 	}
 
+	public List<Turismo> get() {
+		return new ArrayList<>(turismos);
+	}
+
+	public int getCantidad() {
+		return turismos.size();
+	}
+
+	public void insertar(Turismo turismo) {
+		if (turismo != null && !turismos.contains(turismo)) {
+			turismos.add(turismo);
+		}
+	}
+
+	public Turismo buscar(Turismo turismo) {
+		int index = turismos.indexOf(turismo);
+		if (index != -1) {
+			return turismos.get(index);
+		}
+		return null;
+	}
+
+	public void borrar(Turismo turismo) throws Exception {
+		if (turismos.contains(turismo)) {
+			turismos.remove(turismo);
+		} else {
+			throw new Exception("El turismo no se encuentra en la lista.");
+		}
+	}
 }
