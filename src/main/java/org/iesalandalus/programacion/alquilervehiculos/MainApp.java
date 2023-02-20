5876ccc81eef83a711697d83195208f3e1423855
josem <josem@192.168.56.1>
2023-02-20 19:24:21 +0100

    He creado el mainapp

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -7,7 +7,9 @@ import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
 public class MainApp {
 
 	public static void main(String[] args) {
-		// Ánimo!!!!
+		Vista vista = new Vista();
+		Controlador controlador = new Controlador(new Modelo(), vista);
+		controlador.comenzar();
 	}
 
 }
