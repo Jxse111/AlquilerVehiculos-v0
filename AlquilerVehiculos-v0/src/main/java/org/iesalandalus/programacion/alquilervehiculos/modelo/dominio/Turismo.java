5709393c37b1b935fedd8bcebb74b02dade4094f
Jxse111 <josemartinezestrada111@gmail.com>
2023-02-21 11:50:14 +0100

    Ejercicio finalizado

---
+++ AlquilerVehiculos-v0/src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/dominio/Turismo.java
@@ -0,0 +1,109 @@
+package org.iesalandalus.programacion.alquilervehiculos.modelo.dominio;
+
+public class Turismo {
+	private static final String ER_MARCA = "(Seat|Land Rover|KIA|Rolls-Royce|SsangYong)";
+	private static final String ER_MATRICULA = "[0-9]{4}[BCDFGHJKLMNÑPQRSTVWXYZ]{3}";
+	private String marca;
+	private String modelo;
+	private int cilindrada;
+	private String matricula;
+
+	public Turismo(String marca, String modelo, int cilindrada, String matricula) throws IllegalArgumentException {
+		setMarca(marca);
+		setModelo(modelo);
+		setCilindrada(cilindrada);
+		setMatricula(matricula);
+	}
+
+	public Turismo(Turismo turismo) {
+		if (turismo == null) {
+			throw new NullPointerException("ERROR: No es posible copiar un turismo nulo.");
+		}
+		{
+			this.marca = turismo.marca;
+			this.modelo = turismo.modelo;
+			this.cilindrada = turismo.cilindrada;
+			this.matricula = turismo.matricula;
+		}
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) throws IllegalArgumentException {
+		if (marca == null) {
+			throw new NullPointerException("ERROR: la marca no puede ser nula.");
+		}
+
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) throws IllegalArgumentException {
+		if (modelo != null && !modelo.isBlank()) {
+			this.modelo = modelo;
+		}
+		{
+			throw new IllegalArgumentException("Modelo no puede estar en blanco");
+		}
+	}
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(int cilindrada) throws IllegalArgumentException {
+		if (cilindrada >= 0 && cilindrada <= 5000) {
+			this.cilindrada = cilindrada;
+		}
+		{
+			throw new IllegalArgumentException("Cilindrada no es correcta");
+		}
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) throws IllegalArgumentException {
+		if (matricula.matches(ER_MATRICULA)) {
+			this.matricula = matricula;
+		}
+		throw new IllegalArgumentException("Matrícula no tiene un formato válido");
+	}
+
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		Turismo turismo = (Turismo) o;
+
+		return matricula.equals(turismo.matricula);
+	}
+
+	public int hashCode() {
+		return matricula.hashCode();
+	}
+
+	public String toString() {
+		return "Turismo [marca=" + marca + ", modelo=" + modelo + ", cilindrada=" + cilindrada + ", matricula="
+				+ matricula + ", getMarca()=" + getMarca() + ", getModelo()=" + getModelo() + ", getCilindrada()="
+				+ getCilindrada() + ", getMatricula()=" + getMatricula() + ", hashCode()=" + hashCode()
+				+ ", getPrecioDiario()=" + getPrecioDiario() + ", getClass()=" + getClass() + ", toString()="
+				+ super.toString() + "]";
+	}
+
+	public static Turismo getTurismoConMatricula(String matricula) {
+		return new Turismo ("Seat","León", 90, matricula);
+	}
+
+	public int getPrecioDiario() {
+		return 0;
+		
+	}
+}
---
+++ AlquilerVehiculos-v0/src/test/java/org/iesalandalus/programacion/alquilervehiculos/modelo/dominio/ClienteTest.java
@@ -0,0 +1,126 @@
+package org.iesalandalus.programacion.alquilervehiculos.modelo.dominio;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ClienteTest {
+	
+	private static final String MENSAJE_ERROR_NOMBRE_NULO = "ERROR: El nombre no puede ser nulo.";
+	private static final String MENSAJE_ERROR_FORMATO_NOMBRE_NO_VALIDO = "ERROR: El nombre no tiene un formato válido.";
+	private static final String MENSAJE_ERROR_DNI_NULO = "ERROR: El DNI no puede ser nulo.";
+	private static final String MENSAJE_ERROR_FORMATO_DNI_NO_VALIDO = "ERROR: El DNI no tiene un formato válido.";
+	private static final String MENSAJE_ERROR_LETRA_DNI_NO_VALIDA = "ERROR: La letra del DNI no es correcta.";
+	private static final String MENSAJE_ERROR_TELEFONO_NULO = "ERROR: El teléfono no puede ser nulo.";
+	private static final String MENSAJE_ERROR_FORMATO_TELEFONO_NO_VALIDO = "ERROR: El teléfono no tiene un formato válido.";
+	private static final String MENSAJE_ERROR_CLIENTE_NULO = "ERROR: No es posible copiar un cliente nulo.";
+
+	private static final String NOMBRE_VALIDO = "Bob Esponja";
+	private static final String DNI_VALIDO = "11223344B";
+	private static final String TELEFONO_VALIDO = "950112233";
+	
+	private Cliente cliente;
+	
+	
+	void init() {
+		cliente = new Cliente(NOMBRE_VALIDO, DNI_VALIDO, TELEFONO_VALIDO);
+	}
+
+	
+	void constructorNombreValidoDniValidoTelefonoValidoCreaClienteCorrectamente() {
+		assertEquals(NOMBRE_VALIDO, cliente.getNombre());
+		assertEquals(DNI_VALIDO, cliente.getDni());
+		assertEquals(TELEFONO_VALIDO, cliente.getTelefono());
+		cliente = new Cliente("Bob", DNI_VALIDO, TELEFONO_VALIDO);
+	}
+	
+	
+	void constructorNombreNoValidoDniValidoTelefonoValidoLanzaExcepcion() {
+		NullPointerException npe = assertThrows(NullPointerException.class, () -> new Cliente(null, DNI_VALIDO, TELEFONO_VALIDO));
+		assertEquals(MENSAJE_ERROR_NOMBRE_NULO, npe.getMessage());
+		IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> new Cliente("", DNI_VALIDO, TELEFONO_VALIDO));
+		assertEquals(MENSAJE_ERROR_FORMATO_NOMBRE_NO_VALIDO, iae.getMessage());
+		iae = assertThrows(IllegalArgumentException.class, () -> new Cliente(" ", DNI_VALIDO, TELEFONO_VALIDO));
+		assertEquals(MENSAJE_ERROR_FORMATO_NOMBRE_NO_VALIDO, iae.getMessage());
+		iae = assertThrows(IllegalArgumentException.class, () -> new Cliente("bob esponja", DNI_VALIDO, TELEFONO_VALIDO));
+		assertEquals(MENSAJE_ERROR_FORMATO_NOMBRE_NO_VALIDO, iae.getMessage());
+		iae = assertThrows(IllegalArgumentException.class, () -> new Cliente("BOB ESPONJA", DNI_VALIDO, TELEFONO_VALIDO));
+		assertEquals(MENSAJE_ERROR_FORMATO_NOMBRE_NO_VALIDO, iae.getMessage());
+		iae = assertThrows(IllegalArgumentException.class, () -> new Cliente("Bob  Esponja", DNI_VALIDO, TELEFONO_VALIDO));
+		assertEquals(MENSAJE_ERROR_FORMATO_NOMBRE_NO_VALIDO, iae.getMessage());
+	}
+	
+
+	void constructorNombreValidoDniNoValidoTelefonoValidoLanzaExcepcion() {
+		NullPointerException npe = assertThrows(NullPointerException.class, () -> new Cliente(NOMBRE_VALIDO, null, TELEFONO_VALIDO));
+		assertEquals(MENSAJE_ERROR_DNI_NULO, npe.getMessage());
+		IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> new Cliente(NOMBRE_VALIDO, "12345678", TELEFONO_VALIDO));
+		assertEquals(MENSAJE_ERROR_FORMATO_DNI_NO_VALIDO, iae.getMessage());
+		iae = assertThrows(IllegalArgumentException.class, () -> new Cliente(NOMBRE_VALIDO, "12345678X", TELEFONO_VALIDO));
+		assertEquals(MENSAJE_ERROR_LETRA_DNI_NO_VALIDA, iae.getMessage());
+	}
+	
+	
+	void constructorNombreValidoDniValidoTelefonoNoValidoLanzaExcepcion() {
+		NullPointerException npe = assertThrows(NullPointerException.class, () -> new Cliente(NOMBRE_VALIDO, DNI_VALIDO, null));
+		assertEquals(MENSAJE_ERROR_TELEFONO_NULO, npe.getMessage());
+		IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> new Cliente(NOMBRE_VALIDO, DNI_VALIDO, "12345678"));
+		assertEquals(MENSAJE_ERROR_FORMATO_TELEFONO_NO_VALIDO, iae.getMessage());
+		iae = assertThrows(IllegalArgumentException.class, () -> new Cliente(NOMBRE_VALIDO, DNI_VALIDO, "1234567890"));
+		assertEquals(MENSAJE_ERROR_FORMATO_TELEFONO_NO_VALIDO, iae.getMessage());
+	}
+	
+	
+	void constrctorClienteValidoCopiaClienteCorrectamente() {
+		Cliente clienteCopia = new Cliente(cliente);
+		assertEquals(cliente, clienteCopia);
+		assertNotSame(cliente, clienteCopia);
+		assertEquals(NOMBRE_VALIDO, clienteCopia.getNombre());
+		assertEquals(DNI_VALIDO, clienteCopia.getDni());
+		assertEquals(TELEFONO_VALIDO, clienteCopia.getTelefono());
+	}
+
+	
+	void constructorClienteNuloLanzaExcepcion() {
+		NullPointerException npe = assertThrows(NullPointerException.class, () -> new Cliente(null));
+		assertEquals(MENSAJE_ERROR_CLIENTE_NULO, npe.getMessage());
+	}
+	
+	
+	void getClienteConDniValidoDevuelveClienteConDichoDni() {
+		Cliente cliente = Cliente.getClienteConDni(DNI_VALIDO);
+		assertEquals(DNI_VALIDO, cliente.getDni());
+	}
+	
+	 
+	void getClienteConDniNoValidoLanzaExcepcion() {
+		NullPointerException npe = assertThrows(NullPointerException.class, () -> Cliente.getClienteConDni(null));
+		assertEquals(MENSAJE_ERROR_DNI_NULO, npe.getMessage());
+		IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> Cliente.getClienteConDni("12345678"));
+		assertEquals(MENSAJE_ERROR_FORMATO_DNI_NO_VALIDO, iae.getMessage());
+		iae = assertThrows(IllegalArgumentException.class, () -> Cliente.getClienteConDni("12345678X"));
+		assertEquals(MENSAJE_ERROR_LETRA_DNI_NO_VALIDA, iae.getMessage());
+	}
+	
+	
+	void equalsYHasCodeConsistentes() {
+		Cliente cliente = new Cliente(NOMBRE_VALIDO, DNI_VALIDO, TELEFONO_VALIDO);
+		Cliente clienteIgual = Cliente.getClienteConDni(DNI_VALIDO);
+		assertEquals(cliente, cliente);
+		assertEquals(cliente, clienteIgual);
+		assertEquals(clienteIgual, cliente);
+		assertEquals(cliente.hashCode(), clienteIgual.hashCode());
+		Cliente clienteDiferente = new Cliente(NOMBRE_VALIDO, "11111111H", TELEFONO_VALIDO);
+		assertNotEquals(cliente, "");
+		assertNotEquals(cliente, clienteDiferente);
+		assertNotEquals(cliente.hashCode(), clienteDiferente.hashCode());
+		assertNotEquals(cliente, null);
+	}
+	
+	
+	void toStringDevuelveLaCadenaEsperada() {
+		assertEquals(String.format("%s - %s (%s)", NOMBRE_VALIDO, DNI_VALIDO, TELEFONO_VALIDO), cliente.toString());
+	}
+
+}
